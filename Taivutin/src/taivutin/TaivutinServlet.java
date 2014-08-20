5c0ea1ee26f92bd5f024be6a52d45f950251c9a2
Nikolay <vassilev.nikolay@gmail.com>
2014-08-20 12:37:42 +0300

    Initial functionality complete - lunch break...

--- Taivutin/src/taivutin/TaivutinServlet.java
+++ Taivutin/src/taivutin/TaivutinServlet.java
@@ -1,13 +1,23 @@
 package taivutin;
 
 import java.io.IOException;
+
 import javax.servlet.http.*;
+import javax.servlet.ServletException;
 
 @SuppressWarnings("serial")
 public class TaivutinServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+			throws IOException, ServletException {
+		req.getRequestDispatcher("/jsp/subadtaivutin.jsp").forward(req, resp);
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException{
+		
+		resp.setContentType("text/html;charset=UTF-8");
+		String s = req.getParameter("word");
+		System.out.println(s);
+		req.getRequestDispatcher("/jsp/subadtaivutin.jsp").forward(req, resp);
 	}
 }
---
+++ Taivutin/war/jsp/subadtaivutin.jsp
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="ISO-8859-1" ?>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Adjektiivi/Substantiivi-taivutin</title>
+</head>
+<body>
+<form action="taivutin" method="POST"> 
+	Syötä substantiivi/adjektiivi perusmuodossa:  
+	<input type="text" name="word" id="word"/> <br/>
+    <input type="submit", value="Taivuta"/>    		
+		</form>
+</body>
+</html>
